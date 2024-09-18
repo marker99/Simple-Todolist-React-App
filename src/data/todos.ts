55e774d51e5142d3a495c5c5ad999acdcc7891b8
Frederik <frederik.m.hansen@gmail.com>
2024-09-18 21:21:02 +0200

    initial

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData : Todo[] = [
+  {
+    id: 1,
+    title: "Like this video",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Subscribe for more",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Leave a nice commment",
+    completed: false,
+  },
+];
