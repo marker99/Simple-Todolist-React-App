b135f656555404285174611b64df59e4f430a587
Frederik <frederik.m.hansen@gmail.com>
2024-09-19 11:18:40 +0200

    add new todos, can check them and then sort them, delete them

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/todos";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -11,20 +12,29 @@ function App() {
     );
   }
 
+function addTodo (title: string) {
+  setTodos(prevTodos => [
+    {
+      id: prevTodos.length + 1,
+      title,
+      completed: false
+    },
+    ...prevTodos
+  ])
+}
+
+function deleteTodo (id:number){
+  setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+}
+
   return (
-    <main className="py-10 h-screen space-y-5">
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
 
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
-        <div className="space-y-2">
-          {todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              onCompletedChange={setTodoCompleted}
-            />
-          ))}
-        </div>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo}/>
+
+        <TodoList todos={todos} onCompletedChange={setTodoCompleted} onDelete={deleteTodo}/>
       </div>
     </main>
   );
