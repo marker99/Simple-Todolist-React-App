db8185e72b0c33b377ded056295ee18796f72578
Frederik Marker Hansen <frederik.m.hansen@gmail.com>
2024-09-20 21:09:51 +0200

    finished the project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Simple Todolist learning project
 
 A simple todolist project created while learning how to use React, Vite, Tailwind CSS and Typescript.
+
+![Image of the app](image.png)
--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,32 @@
-import { useState } from "react";
-import { dummyData } from "./data/todos";
 import AddTodoForm from "./components/AddTodoForm";
 import TodoList from "./components/TodoList";
+import TodoSummary from "./components/TodoSummary";
+import useTodos from "./hooks/useTodos";
 
 function App() {
-  const [todos, setTodos] = useState(dummyData);
-
-  function setTodoCompleted(id: number, completed: boolean) {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
-    );
-  }
-
-function addTodo (title: string) {
-  setTodos(prevTodos => [
-    {
-      id: prevTodos.length + 1,
-      title,
-      completed: false
-    },
-    ...prevTodos
-  ])
-}
-
-function deleteTodo (id:number){
-  setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
-}
+  const {
+    todos,
+    addTodo,
+    setTodoCompleted,
+    deleteTodo,
+    deleteAllCompletedTodos,
+  } = useTodos();
 
   return (
     <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
 
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
-        <AddTodoForm onSubmit={addTodo}/>
+        <AddTodoForm onSubmit={addTodo} />
 
-        <TodoList todos={todos} onCompletedChange={setTodoCompleted} onDelete={deleteTodo}/>
+        <TodoList
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+        />
       </div>
+
+      <TodoSummary todos={todos} deleteAllCompleted={deleteAllCompletedTodos} />
     </main>
   );
 }
